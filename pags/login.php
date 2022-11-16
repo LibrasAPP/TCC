9aaec174d62a054217b8ff34740a678a1e864745
Luiz Felipe dos Santos <felipe.pinghang2@gmail.com>
2022-11-16 20:52:27 -0300

    teste

--- pags/login.php
+++ pags/login.php
@@ -1,41 +1,41 @@
-<?php
-
-    session_start();
-    include('conexao.php');
-    $_SESSION['logado'] = false;
-
-  /*  if(isset($_POST['email']) || isset($_POST['senha'])) {
-        if(strlen($_POST['email'])== 0){
-            echo "Preencha seu e-mail!";
-        }elseif(strlen($_POST['senha'])== 0){
-            echo "Preencha sua senha!";
-        }else{*/
-            if(isset($_POST['email']) && isset($_POST['senha'])) {    
-                $email =$_POST['email'];
-                $senha =$_POST['senha'];
-            
-        
-                $sql_code = "SELECT * FROM usuarios WHERE Emailusuario = '".$email."' AND senha ='".$senha."'";
-                $sql_query = $mysqli->query($sql_code) or die('Falha na execução do código SQL'.$mysqli->error);
-
-                $quantidade=$sql_query->num_rows;
-
-                if($quantidade> 0){
-                    $usuario = $sql_query->fetch_assoc();
-
-                    
-
-                    $_SESSION['idusuario'] = $usuario['idusuario'];
-                    $_SESSION['logado'] = true;
-                    $_SESSION['email'] = $email;
-                // $_SESSION['nome'] = $usuario['nome'];
-                header("Location:logado.php");
-                }else{
-                    echo 'Falha ao logar! E-mail ou senha incorretos!';
-                    session_destroy();
-                }
-        
-        }
-
-?>
-
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/login.css">
+    <title>Entre na sua conta!</title>
+</head>
+<body>
+    <main>
+        <div id="top">
+            <h1>Entrar</h1>
+        </div>
+        <form method="post" action="login.php">
+            <div id="center">
+                <?php include('errors.php'); ?>
+                <!-- <div id="error">
+                    <p>Username or password incorrect</p>
+                </div> -->
+                <div id="login-password">
+                    <input class="input-grout" type="text" name="username" placeholder="Nome de Usuário">
+                    <input class="input-grout" type="password" name="password" placeholder="Senha">
+                </div>
+                <div id="registro">
+                    <p>Ainda não tem registro? <a href="register.php">Registre-se!</a></p>
+                </div>
+            </div>
+            <div id="botton">
+                <div id="back">
+                    <div><a href="#">Voltar</a></div>
+                </div>
+                <div id="enter">
+                    <button type="submit" name="login_user">Entrar</button>
+                </div>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
